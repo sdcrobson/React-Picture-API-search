8da6b55aea9f808be47eead04cc73f3cb0c49518
sdcrobson <sdcrobson@me.com>
2021-02-09 15:19:39 -0600

    updating search bar to handle user input

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -6,21 +6,39 @@ if (module.hot) {
 
 //class componen -> has a state and render function!
 class SearchBar extends React.Component {
-  //function to handle user input to be sent to the API
+  state = { term: "" };
+  /*function to handle user input to be sent to the API
   onInputChange(event) {
     console.log(event.target.value);
   }
+  */
+  //fixes the this is undefined error
+  onFormSubmit = (event) => {
+    //stops browser from refreshing the page
+    event.preventDefault();
+    //fixing the this problem is undefined
+    console.log(this.state.term);
+  };
+
+  // can also use // arrow function used instead of created seperate function
+  //onChange={(e) => console.log(e.target.value)}
+
   render() {
     //multiline jsx - remember to remove the ; from the end of the div tag!  it wont work!
     return (
       //using semantic ui classNames to style the searchbar
       //this.onInputChange is left without() - saves being automatically called everytime its render
       // want it to be called sometime in the future - passing reference to the element
+      //onChange import property used to detect user changes text input - Also onClick and onSubmit
       <div className="ui segment">
-        <form className="ui form">
+        <form onSubmit={this.onFormSubmit} className="ui form">
           <div className="field">
             <label>Image Search</label>
-            <input type="text" onChange={this.onInputChange}></input>
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={(e) => this.setState({ term: e.target.value })}
+            ></input>
           </div>
         </form>
       </div>
