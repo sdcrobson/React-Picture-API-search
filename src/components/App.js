dc3f4f0494105211ada6f6b414aa35a3949a2b46
sdcrobson <sdcrobson@me.com>
2021-02-09 19:44:45 -0600

    adding unslplash api and axios library

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,31 @@
 import React from "react";
+import axios from "axios";
 import SearchBar from "./SearchBar";
+
 //parent level in the heirarchy
 
 if (module.hot) {
   module.hot.accept();
 }
 //functional components (no class)
-const App = () => {
-  //returns the searchbar component - allows for easy reuse!
-  return (
-    //classname allows for searchbar to be wrapped in container - also using inline styling for just on css style
-    <div className="ui container" style={{ marginTop: 10 }}>
-      <SearchBar />
-    </div>
-  );
-};
+class App extends React.Component {
+  async onSearchSubmit(term) {
+    const response = await axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization: "Client-ID DyM0i9Hjhq9cIiVu_s0KOT8GeD05cKMQBS558uFIo_M",
+      },
+    });
+    console.log(response.data.results);
+  }
+  render() {
+    return (
+      //classname allows for searchbar to be wrapped in container - also using inline styling for just on css style
+      <div className="ui container" style={{ marginTop: 10 }}>
+        <SearchBar onSubmit={this.onSearchSubmit} />
+      </div>
+    );
+  }
+}
 
 export default App;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,32 +4,41 @@ if (module.hot) {
   module.hot.accept();
 }
 
-//class componen -> has a state and render function!
+//class component -> has a state and render function!
 class SearchBar extends React.Component {
   state = { term: "" };
+
   /*function to handle user input to be sent to the API
   onInputChange(event) {
     console.log(event.target.value);
   }
   */
-  //fixes the this is undefined error
+  /*handles the form submission event -> takes the event
+    prevents default behaviour from happening(refreshing)
+    then takes the prop object and invokes the onsubmit -> App.js handles this
+  */
   onFormSubmit = (event) => {
     //stops browser from refreshing the page
     event.preventDefault();
     //fixing the this problem is undefined
-    console.log(this.state.term);
+    //console.log(this.state.term);
+    //reference to object that was passed in on line 36
+    this.props.onSubmit(this.state.term);
   };
 
-  // can also use // arrow function used instead of created seperate function
-  //onChange={(e) => console.log(e.target.value)}
-
+  /*
+    can also use // arrow function used instead of created seperate function
+    onChange={(e) => console.log(e.target.value)}
+  */
   render() {
     //multiline jsx - remember to remove the ; from the end of the div tag!  it wont work!
     return (
-      //using semantic ui classNames to style the searchbar
-      //this.onInputChange is left without() - saves being automatically called everytime its render
-      // want it to be called sometime in the future - passing reference to the element
-      //onChange import property used to detect user changes text input - Also onClick and onSubmit
+      /*
+        using semantic ui classNames to style the searchbar
+        this.onInputChange is left without() - saves being automatically called everytime its render
+        want it to be called sometime in the future - passing reference to the element
+        onChange import property used to detect user changes text input - Also onClick and onSubmit
+      */
       <div className="ui segment">
         <form onSubmit={this.onFormSubmit} className="ui form">
           <div className="field">
